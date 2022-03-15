5a9c619a16c2b413c083bbed0cde9199b7bab965
Igor Braga Palhano <94334598+BPalhano@users.noreply.github.com>
2022-03-15 15:14:40 -0300

    Update simulator.py

--- Simulador/simulator.py
+++ Simulador/simulator.py
@@ -24,46 +24,6 @@ sigRay = (1/ np.sqrt(2))
 
 #  métodos
 
-def antenas(num, dist):  # Gerador de posicionamento de ERB's
-
-    x = dist * random.rand()
-    y = dist * random.rand()
-
-    vetor1 = np.array([0,0])
-    vetor2 = np.array([x,y])
-  
-    contador = 0
-    while (contador != num):
-
-        x = dist * random.rand()
-        y = dist * random.rand()
-      
-        arr1 = np.array([dist* math.cos((2*math.pi/num) * contador) , dist* math.sin((2*math.pi/num) * contador)])
-        arr2 = np.array([arr1[0] + x, arr1[1] + y])
-        
-        vetor1 = np.vstack((vetor1, arr1))
-        vetor2 = np.vstack((vetor2,arr2))
-        
-        contador += 1
-
-    
-    return vetor1, vetor2
-
-
-def distancia(vet1,vet2): #  distância analítica entre dois corpos.
-
-    d = np.array([])
-
-    for pos in it:
-
-        vetA = vet1[pos]
-        vetB = vet2[pos]
-
-        aux = math.sqrt(( (vetA[0] - vetB[0]) **2   + (vetA[1] - vetB[1]) **2 ))
-        d = np.hstack((d,aux))
-
-    return d    #  saida é um vetor coluna n+1 x 1 
-
 def lognorm(sigma):  #  Gerador de números log-normais.
 
     x = random.rand() * sigma
@@ -97,50 +57,66 @@ def sig(vet1):  #  desvio padrão de valores em um vetor qualquer.
 
     return sigma
 
-def Pn(vet1, vet2):  # Função geradora do ganho em W da fonte. (Ganho linear)
+def antenas(num, dist):  # Gerador de posicionamento de ERB's
 
-    P = np.array([0])
-    d = distancia(vet1,vet2)
+    global sigLN
+    global sigRay
 
-    for i in range(len(vet1)):
+    x = dist * random.rand()
+    y = dist * random.rand()
 
-        P = np.vstack((P, [128.1 + 36.7 + np.log10(d[i])])) #  P gerado primeiramente em dB.
+    vetor1 = np.array([0,0])
+    vetor2 = np.array([x,y])
+    vetor3 = np.array([0])
+    vetor4 = np.array([0])
+    vetor5 = np.array([0])
 
-    for i in P:
+    contador = 0
+    while (contador != num):
 
-        i = 10 ** (i/10)  #  e então convertido para escala linear.
+        x = dist * random.rand()
+        y = dist * random.rand()
+      
+        arr1 = np.array([dist* math.cos((2*math.pi/num) * contador) , dist* math.sin((2*math.pi/num) * contador)])
+        arr2 = np.array([arr1[0] + x, arr1[1] + y])
+        
+        d = pow(arr1[0] - arr2[0],2) + pow(arr1[1] - arr2[1], 2)
+        d = math.sqrt(d)
 
-    P = P[1:len(P) -1]
+        arr3 = np.array([128.1 + 36.7 + np.log10(d)]) # dB
+        arr4 = np.array([10**(lognorm(sigLN) /10)]) # dB
+        arr5 = np.array([sigRay*dray(sigRay,0)])
 
-    return P
+        vetor1 = np.vstack((vetor1, arr1))
+        vetor2 = np.vstack((vetor2,arr2))
 
-def Sombreamento(vet1,sd): #  Função geradora para valores de sombreamento em W:
+        vetor3 = np.vstack((vetor3,arr3))
+        vetor4 = np.vstack((vetor4, arr4))
+        vetor5 = np.vstack((vetor5,arr5))
 
-    S = np.array([0])
+        contador += 1
 
-    for element in vet1:
 
-        x = 10**(lognorm(sd) /10)
-        S = np.vstack((S, x))
+    vetor4[0] = 10 **(lognorm(sigLN)/10)
+    vetor5[0] = sigRay *dray(sigRay,0)
 
-    S = S[1: len(vet1) -1]
-    return S
-            
-def Desvanecimento(vet1, sd): #  Função geradora para valores de Desvanecimento rápido em W. 
-
-    D = np.array([0])
-
-    for element in vet1:
-
-        x = sd*dray(sd,0)
-        D = np.vstack((D, x))
     
-    D = D[1: len(vet1) - 1]
-
-    return D
+    return vetor1, vetor2,vetor3, vetor4, vetor5
 
 
+def distancia(vet1,vet2): #  distância analítica entre dois corpos.
 
+    d = np.array([])
+
+    for pos in it:
+
+        vetA = vet1[pos]
+        vetB = vet2[pos]
+
+        aux = math.sqrt(( (vetA[0] - vetB[0]) **2   + (vetA[1] - vetB[1]) **2 ))
+        d = np.hstack((d,aux))
+
+    return d    #  saida é um vetor coluna n+1 x 1 
 #  main
 
 #  Simulação:  
@@ -162,17 +138,14 @@ for i in loop:
     G = np.array([])
 
     #  Mudança de valores devido a atualização do estado do sistema.
-    ERB, TM = antenas(N, R)
+    ERB, TM, P, S, D = antenas(N, R)
     
-    P = Pn(TM,ERB)  # Vetor coluna n + 1 x 1   em W
-    S = Sombreamento(ERB,sigLN) # Vetor coluna n+1 x 1 com todos os valores para ERBi TMi  em dB
-    D = Desvanecimento(ERB, sigRay)  # vetor coluna n+1 x 1 com tods os Fast fadding para ERBi e TMi  em dB
     
 
 
 
 
-print(ERB, '\n\n', TM, '\n\n', P , '\n\n', S, '\n\n', D, '\n\n', '\n\n',N)
+print(ERB, '\n\n', TM, '\n\n', P , '\n\n', S, '\n\n', D, '\n\n', '\n\n',N, '\n\n')
 
 
 
