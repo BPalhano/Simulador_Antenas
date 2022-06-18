5d646936702dbda6c872893c5adb9ef9135e5996
Igor Braga Palhano <94334598+BPalhano@users.noreply.github.com>
2022-06-17 21:23:12 -0300

    Update simulator.py

--- Simulador/simulator.py
+++ Simulador/simulator.py
@@ -1,78 +1,29 @@
+################################################ BIBLIOTECAS ###########################################################
 import numpy as np
 import pandas as pd
+import seaborn as sbn
+from scipy import stats as scs
+from matplotlib import pyplot as plt
 from numpy import random
 import math
-import matplotlib.pyplot as plt
-from scipy.stats import rice
-import seaborn as sbn
+########################################################################################################################
 
-#  Configuraçao inicial
-
-
-# É preciso primeiramente declarar variáveis globais.
-
-R = int(input('Insira o Raio de alcance das antenas: '))
-N = int(input('Insira o número de antenas desejado: '))
-loop = int(input('Insira o número de loops que deve acontecer na simulação: '))
+############################################  VARIÁVEIS GLOBAIS ########################################################
+N = int(input('Numero de usuários: '))
+R = 2000
+loop = int(input('Quantos laços de repetição: '))
 
 print('\n\n')
 
+c = 299792458
+freq = 6000000000
+_lambda = c / freq
 loop = range(loop)
 it = range(N + 1)
-
-PT = 19.95262315  # 43 dBm
-PN = (2.5118864315 * 10 ** (-15))  # linear scale
-sigLN = 8  # dB
-
 SINR_final = np.array([])
+########################################################################################################################
 
-
-def antenas(num, dist):  # Gerador de posicionamento de ERB's
-
-    x = R * math.sqrt(random.rand()) * random.rand() * 2 * math.pi
-    y = R * math.sqrt(random.rand()) * random.rand() * 2 * math.pi
-
-    vetor1 = np.array([0, 0])
-    vetor2 = np.array([x, y])
-
-    contador = 0
-
-    while (contador != num):
-        x = R * math.sqrt(random.rand()) * random.rand() * 2 * math.pi
-        y = R * math.sqrt(random.rand()) * random.rand() * 2 * math.pi
-
-        arr1 = np.array(
-            [dist * math.cos((2 * math.pi / num) * contador), dist * math.sin((2 * math.pi / num) * contador)])
-        arr2 = np.array([arr1[0] + x, arr1[1] + y])
-
-        vetor1 = np.vstack((vetor1, arr1))
-        vetor2 = np.vstack((vetor2, arr2))
-
-        contador += 1
-
-    return vetor1, vetor2
-
-
-def lognorm(sigma):  # Gerador de números log-normais.
-
-    x = random.rand() * sigma
-
-    return x
-
-
-def matriz_dist(vet1, vet2):  # matriz de distâncias de ERB i para TM j
-
-    d = np.array([])
-
-    for i in vet1:
-
-        for j in vet2:
-            dist = math.sqrt(pow((i[0] - j[0]), 2) + pow((i[1] - j[1]), 2))
-            d = np.hstack((d, dist))
-
-    return d  # retorna uma matriz i x j
-
-
+# #################################### CONVERSOR DE UNIDADES ###########################################################
 def dB_to_linear(x):  # conversor de dB para escala linear
 
     x = 10 ** (x / 10)
@@ -87,36 +38,97 @@ def linear_to_dB(x):  # conversor de escala linear para dB
     return x  # retorno em dB
 
 
-def Pot_linear(vet1):  # Gerador de interferência linear.
+def Soma(vet1):  # Função para somar todas os elementos de um vetor
+
+    s = 0
+
+    for i in vet1:
+        s += i
+
+    return s
+########################################################################################################################
+
+
+# #################################### GERADORES DE POSICIONAMENTO #####################################################
+def ERB_TM(num, dist):
+    ERB = np.array([0, 0, 100])
+    TM = np.array([R * random.uniform(0, 1), R * random.uniform(0, 1), 0])
+
+    contador_ERB = 0
+
+    while contador_ERB != 6:
+        arr1 = np.array(
+            [dist * math.cos((2 * math.pi / 6) * contador_ERB), dist * math.sin((2 * math.pi / 6) * contador_ERB), 100])
+        ERB = np.vstack((ERB, arr1))
+
+        arr2 = np.array(
+            [arr1[0] + dist * random.uniform(0, 1), arr1[1] + dist * random.uniform(0, 1), 100 * random.uniform(-1, 0)])
+        TM = np.vstack((TM, arr2))
+
+        contador_ERB += 1
+
+    if N > contador_ERB:
+        contador_TM = 0
+
+        while contador_TM != num:
+            aux = np.array([6 * dist * random.normal(0, 1), 6 * dist * math.sin(math.pi / 3) * random.uniform(0, 1),
+                            100 * random.uniform(0, 1)], dtype=object)
+
+            TM = np.vstack((TM, aux))
+            contador_TM += 1
+
+    return ERB, TM
+
+
+def matriz_dist(vet1, vet2):  # matriz de distâncias de ERB i para TM j
+
+    d = np.array([])
+
+    for i in vet1:
+
+        for j in vet2:
+            dist = math.sqrt(pow((i[0] - j[0]), 2) + pow((i[1] - j[1]), 2) + pow((i[2] - j[2]), 2))
+            d = np.hstack((d, dist))
+
+    return d  # retorna uma matriz i x j
+########################################################################################################################
+
+
+# ################################# GERADORES DE RUIDO #################################################################
+
+
+def Path_loss(vet1):  # Gerador de interferência linear.
 
     vet1 = vet1.copy()
 
     for i in range(len(vet1)):
-        vet1[i] = dB_to_linear(164.8 + np.log10(vet1[i]))
+        vet1[i] = dB_to_linear(20 * np.log10(4 * math.pi * vet1 / _lambda))
 
     return vet1  # em W
 
 
-def Sombreamento(vet1, sd):  # Gerador de Sombreamento.
-
+def Sombreamento_teste(vet1, sd):
     aux = vet1.copy()
 
+    rv = random.lognormal(mean=0, sigma=8)
+
+    while rv > 100:
+        rv = random.lognormal(mean=0, sigma=8)
+
     for i in range(len(vet1)):
-        aux[i] = dB_to_linear(lognorm(sd))
+        aux[i] = dB_to_linear(rv)
 
     return aux  # em W
 
 
 def Fast_fadding(vet1):
-
     aux = vet1.copy()
 
     for i in range(len(vet1)):
-        rv = rice.rvs(float(1 / np.sqrt(2)) , 0 )
+        rv = scs.rice.rvs(10)
         aux[i] = dB_to_linear(rv)
 
-
-    return aux # em W
+    return aux  # em W
 
 
 def Ganho(vet1, vet2, vet3, K):  # Ganho total do sistema
@@ -129,73 +141,48 @@ def Ganho(vet1, vet2, vet3, K):  # Ganho total do sistema
     return G  # em W
 
 
-def Soma(vet1):  # Função para somar todas os elementos de um vetor
-
-    s = 0
-
-    for i in vet1:
-        s += i
-
-    return s
-
-
-def SINR(num, vet1, K):  # Função para calcular o SINR
-
+def SINR(vet1, const):
     matrix = vet1.copy()
-    trace = np.array([])
+    trace = 0
 
-    contador = 0
-    for element in range(len(matrix)):
+    for i in range(0, 7):
+        trace += matrix[7 * i]
 
-        if (contador % (num + 1) == 0):
-            trace = np.hstack((trace, matrix[element]))
-        contador += 1
+    tot = Soma(matrix)
 
-    Sum_trace = Soma(trace)
-    Sum_matrix = Soma(matrix)
-
-    Sum_const = Sum_matrix - Sum_trace
+    cnt = tot - trace
 
     SNR = np.array([])
 
-    for element in range(len(trace)):
-        aux = (trace[element] / (Sum_const + K))
+    for i in range(7):
+        val = matrix[i * 7] / (const + cnt)
 
-        SNR = np.hstack((SNR, aux))
+        SNR = np.hstack((SNR, val))
 
     return SNR
 
-def sbn_eCDF(vet1):
 
+def sbn_eCDF(vet1):
     dataset = vet1.copy()
     df = pd.DataFrame(dataset)
 
     sbn.ecdfplot(df)
-
-    plt.show()
-
-
-#  main
-
-#  Simulação:
-
-for i in loop:  # Simulação
-
-    # Atualização do Estado
-    ERB, TM = antenas(N, R)  # Armazenamento da posição do móveis e antenas.
-
-    P = Pot_linear(matriz_dist(ERB, TM))  # Armazenamento do Ruído Linear do estado atual
-    S = Sombreamento(P, sigLN)  # Armazenamento do Ruído de Sombreamento do estado atual.
-    D = Fast_fadding(P)  # Armazenamento do Ruído de Desvanecimento Rápido do estado atual.
-
-    G = Ganho(P, S, D, PT)  # Armazenamento do Ganho total em W do estado atual.
-
-    #  Organização e Armazenamento Parcial (SINR)
-
-    SINR_final = np.hstack((SINR_final, (SINR(N, G, PN))))
+########################################################################################################################
 
 
-#  Organização e Armazenamento final
-SINR_final = linear_to_dB(SINR_final)
+# #################################################### MAIN ############################################################
+for i in loop:
+    ERB, TM = ERB_TM(N, R)
+    P = matriz_dist(ERB, TM)
+    S = Sombreamento_teste(P, 8)
+    D = Fast_fadding(P)
+    G = Ganho(P, S, D, 19.95262315)
+    SINR_final = np.hstack((SINR_final, SINR(G, 0)))
 
-sbn_eCDF(SINR_final)
+sbn_eCDF(linear_to_dB(SINR_final))
+plt.show()
+########################################################################################################################
+
+################################################## DEBUG ###############################################################
+print(P, '\n\n', S, '\n\n', D, '\n\n', G, '\n\n', SINR_final)
+########################################################################################################################
