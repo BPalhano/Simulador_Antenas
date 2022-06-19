ace3b0fbccf054db9df06e183f159a8fac83ba1a
Igor Braga Palhano <94334598+BPalhano@users.noreply.github.com>
2022-06-19 16:32:34 -0300

    Update simulator.py

--- Simulador/simulator.py
+++ Simulador/simulator.py
@@ -10,7 +10,7 @@ import math
 
 ############################################  VARIÁVEIS GLOBAIS ########################################################
 N = int(input('Numero de usuários: '))
-R = 2000
+R = 200
 loop = int(input('Quantos laços de repetição: '))
 
 print('\n\n')
@@ -21,6 +21,8 @@ _lambda = c / freq
 loop = range(loop)
 it = range(N + 1)
 SINR_final = np.array([])
+SINR_final2 = np.array([])
+
 ########################################################################################################################
 
 # #################################### CONVERSOR DE UNIDADES ###########################################################
@@ -52,13 +54,14 @@ def Soma(vet1):  # Função para somar todas os elementos de um vetor
 # #################################### GERADORES DE POSICIONAMENTO #####################################################
 def ERB_TM(num, dist):
     ERB = np.array([0, 0, 100])
+    ap = dist * math.sqrt(3)/2
     TM = np.array([R * random.uniform(0, 1), R * random.uniform(0, 1), 0])
 
     contador_ERB = 0
 
     while contador_ERB != 6:
         arr1 = np.array(
-            [dist * math.cos((2 * math.pi / 6) * contador_ERB), dist * math.sin((2 * math.pi / 6) * contador_ERB), 100])
+            [2*ap * math.cos((math.pi / 6) + contador_ERB*math.pi/3), 2*ap * math.sin((math.pi / 6) + contador_ERB*math.pi/3), 100])
         ERB = np.vstack((ERB, arr1))
 
         arr2 = np.array(
@@ -110,16 +113,28 @@ def Path_loss(vet1):  # Gerador de interferência linear.
 def Sombreamento_teste(vet1, sd):
     aux = vet1.copy()
 
-    rv = random.lognormal(mean=0, sigma=8)
+    rv = random.lognormal(mean=0, sigma=sd)
 
     while rv > 100:
-        rv = random.lognormal(mean=0, sigma=8)
+        rv = random.lognormal(mean=0, sigma=sd)
 
     for i in range(len(vet1)):
         aux[i] = dB_to_linear(rv)
 
     return aux  # em W
 
+def Sombreamento(vet1, sd):
+
+    aux = vet1.copy()
+
+    for i in range(len(vet1)):
+        rv = random.lognormal(mean=0, sigma=sd)
+
+        while(rv > 100):
+            rv = random.lognormal(mean=0, sigma=sd)
+
+        aux[i] = dB_to_linear(rv)
+    return aux #  em W
 
 def Fast_fadding(vet1):
     aux = vet1.copy()
@@ -175,14 +190,19 @@ for i in loop:
     ERB, TM = ERB_TM(N, R)
     P = matriz_dist(ERB, TM)
     S = Sombreamento_teste(P, 8)
+    S2 = Sombreamento(P,8)
     D = Fast_fadding(P)
     G = Ganho(P, S, D, 19.95262315)
-    SINR_final = np.hstack((SINR_final, SINR(G, 0)))
+    G2 = Ganho(P, S2, D, 19.95262315)
+    SINR_final = np.hstack((SINR_final, SINR(G,0)))
+    SINR_final2 = np.hstack((SINR_final2, SINR(G2,0)))
 
-sbn_eCDF(linear_to_dB(SINR_final))
-plt.show()
+sbn_eCDF(linear_to_dB(SINR_final))  # Curva normalizada
+#sbn_eCDF(linear_to_dB(SINR_final2))  # Curva estranha
+
 ########################################################################################################################
-
+plt.xlabel('SNR(dB)')
+plt.show()
 ################################################## DEBUG ###############################################################
 print(P, '\n\n', S, '\n\n', D, '\n\n', G, '\n\n', SINR_final)
 ########################################################################################################################
